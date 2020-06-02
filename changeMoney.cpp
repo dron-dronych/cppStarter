a59d325efa2c6a1087ec90c0cfeddc58cbab03d7
dron <dr_dronych@mail.ru>
2020-06-03 02:12:37 +0600

    impl algorith to calculate min num of coins to exchange

---
+++ changeMoney.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int changeMoney(int m) {
+	if(m < 1 || m > 1000) {
+		return 1;
+	}
+	int denominations [3] = {1, 5, 10};
+	int remainder = m;
+	int counter = 0;
+
+	while(remainder > 0) {
+		int diffs [3];
+		int ids [3];
+		int pos = 0;
+
+		for(int d=0; d<sizeof(denominations); d++) {
+			if(remainder - d >= 0) {
+				diffs[d] = m - denominations[d];
+				ids[d] = pos;
+				
+				pos++;
+			} else {
+			continue;
+			}
+		}
+		
+		int *min = min_element(begin(diffs), end(diffs));
+		int bestPosition;
+
+		// finding index of best position
+		for (int i=0; i < sizeof(diffs); i++) {
+			if (diffs[i] == *min) {
+				bestPosition = i;
+			}
+		}
+
+		remainder -= denominations[bestPosition];
+		counter++;
+
+	}
+
+	//cout << counter << endl;
+
+	return counter;
+}
+
+int main() {
+	int coins = changeMoney(10);
+	cout << coins << endl;
+	return 0;
+}
