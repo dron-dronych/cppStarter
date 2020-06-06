067a6ca37eee7bd00e5572fab9518cfd6af86a50
dron <dr_dronych@mail.ru>
2020-06-06 15:40:45 +0600

    impl minRefills - compiles properly

--- minRefills.cpp
+++ minRefills.cpp
@@ -1,11 +1,11 @@
-#include <iostrem>
+#include <iostream>
 #include <cstdarg>
 
 using namespace std;
 
 int minRefills(int distance, int capacity, int n_stops, ...) {
 	va_list arguments;
-	int [n_stops] stops;
+	int stops [n_stops];
 
 	for (int i = 0; i < n_stops; i++) {
 		stops[i] = va_arg(arguments, int);
@@ -13,4 +13,30 @@ int minRefills(int distance, int capacity, int n_stops, ...) {
 
 	va_end(arguments);
 
+	int position = 0;
+	int numRefills = 0;
+
+	while (position <= n_stops) {
+		int lastRefill = position;
+
+		while (position <= n_stops && stops[position + 1] - stops[lastRefill] <= capacity) {
+			position++;
+		}
+
+		if (lastRefill == position) {
+			return -1;
+		}
+
+		if (lastRefill <= n_stops) {
+			numRefills++;
+		}
+	}
+
+	return numRefills;
+
+}
+
+int main() {
+	cout << minRefills(950, 400, 4, 200, 375, 550, 750) << endl;
+	return 0;
 }
