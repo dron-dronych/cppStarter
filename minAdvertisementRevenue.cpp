907174227fc55f8c09ce193fa61051ff62b729f6
dron-dronych <dr_dronych@mail.ru>
2020-06-21 23:47:24 +0600

    min advertisement revenue - WIP

---
+++ minAdvertisementRevenue.cpp
@@ -0,0 +1,12 @@
+/* Given two sequences a 1 , a 2 , . . . , a n (a i is the profit per click of the i-th ad) and b 1 , b 2 , . . . , b n (b i is the average number of clicks per day of the i-th slot), we need to partition them into n pairs (a i , b j ) such that the sum of their products is maximized.
+ 
+ implemented with data structures from stdlib
+ */
+ 
+ int minAdvertisementRevenue() {
+ }
+
+ int main() {
+ 	minAdvertisementRevenue();
+ }
+
