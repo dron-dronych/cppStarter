03dd573c8378ccb08c33116d6672d604ef012af0
dron-dronych <dr_dronych@mail.ru>
2020-11-29 23:21:30 +0600

    cpp-style swap using references

---
+++ swap.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/*void swap(int *a, int *b) {
+        int tmp = *a;
+        a = *b;
+        *b = tmp;
+
+}*/
+
+void swapRef(int &a, int &b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int main() {
+	int a = 10;
+	int b = 20;
+
+	printf("a before swapping: %d\n", a);
+	printf("b before swapping: %d\n", b);
+	
+//	swap(&a, &b);
+
+	printf("a after swapping: %d\n", a);
+	printf("b after swapping: %d\n", b);
+
+	swapRef(a, b);
+	printf("a after swapping: %d\n", a);
+	printf("b after swapping: %d\n", b);
+	
+	return 0;
+}
+
